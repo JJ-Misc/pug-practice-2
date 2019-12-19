4cec9aab945bc79f80c8c25b02b3f3564daf9992
github username <github email address>
2019-12-19 12:30:10 -0500

    filling out routes and templates

--- app.js
+++ app.js
@@ -12,9 +12,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Log activity
 app.use(logger('dev'));
+// Use Express's native json() and urlencoded()
+//!NOTE: How is this different from using body-parser?
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// Parse cookies
 app.use(cookieParser());
 
 app.use('/', indexRouter);
@@ -28,10 +32,14 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
+  //!NOTE: In production, don't set res.locals.error to err. Why not?
+  //!NOTE: This line confuses me. Why is app inside the req object?
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
+  // Set the status to the error status or default to generic server error
   res.status(err.status || 500);
+  // Render the error template
   res.render('error');
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const router = express.Router();
+
+/* Require the data containing the recipes */
 const { recipes } = require('../data/data.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // 1. Pass all recipe data to 'index' template
+  res.locals.recipes = recipes;
   res.render('index');
 });
 
 /* GET recipe page. */
 router.get('/recipes/:id', function(req, res, next) {
+  // The request object's params property contains the recipe ID. The request gets this ID from the URL parameter
   const recipeId = req.params.id;
+  // For each recipe ID, find the one with the matching ID
+  //!NOTE: Is the + to turn it into a string?
   const recipe = recipes.find( ({ id }) => id === +recipeId );
   
   if (recipe) {
     // 2. Pass the recipe data to the 'recipe' template
+    res.locals.recipe = recipe;
     res.render('recipe');
   } else {
     res.sendStatus(404);
